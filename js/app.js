ce8a7ad02b3239879b1f5d5914269c15bb6f0538
Christian Fernandez <christian14@gmail.com>
2024-07-27 08:30:28 -0400

    Mostrando los tweets en el HTML / .159

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // Variables
 const formulario = document.querySelector('#formulario');
 const contenedorTweets = document.querySelector('#lista-tweets');
-let tweet = []; // donde se guardara cada tweet
+let tweets = [];
 
 eventListener();
 
@@ -14,49 +14,91 @@ function eventListener() {
 function agregarTweet(event) {
   event.preventDefault();
 
-  // Textarea, donde usuario escribe
   const tweet = document.querySelector('#tweet').value;
 
-  // Validamos
   if (tweet === '') {
-    console.log('No puede eviarse vacio...');
-
     mostrarError('Un mensaje no puede ir vacio');
-
-    return; // recuerda que debes prevenir que siga ejecutandose en caso que entre aca
+    return;
   }
 
-  console.log(tweet, typeof tweet);
+  const tweetObj = {
+    id: Date.now(),
+    tweet: tweet
+  }
+
+  // Una vez pasada la validación, añadimos el cada tweet
+
+  tweets = [...tweets, tweetObj]
+
+  console.log(tweets);
+
+  // Mostramos en el HTML cada Tweet 
+  crearHTML();
+
+  // Reiniciamos el formulario
+  formulario.reset();
 
 }
 
 function mostrarError(error) {
+  const contenedor = document.querySelector('#contenido');
   const mensajeError = document.createElement('P');
+
   mensajeError.classList.add('error')
   mensajeError.textContent = error;
-
-  // Insertamos el error en el HTML
-  const contenedor = document.querySelector('#contenido');
   contenedor.appendChild(mensajeError);
 
-  // Eliminamos la alerta luego de 2 seg.
+
   setTimeout(() => {
     mensajeError.remove()
   }, 2000);
 
+}
 
+function crearHTML() {
+  // console.log('Mostrando tweet...');
+
+  if (tweets.length > 0) {
+    // Limpiamos el HTML para que no se repita la data
+    LimpiarHTML(); // Si comentas esta linea veras el resultado
+
+    tweets.forEach(x => {
+      const { tweet } = x;
+      // Creamos el HTML
+      const li = document.createElement('li');
+
+      // Añadimos el texto
+      li.innerText = tweet;
+
+      // Insertamos en el HTML
+      contenedorTweets.appendChild(li);
+
+
+    });
+  }
+}
+
+// Limpiar el HTML
+function LimpiarHTML() {
+  while (contenedorTweets.firstChild) {
+    contenedorTweets.removeChild(contenedorTweets.firstChild)
+  }
 }
 
 
 /** 
  * *Comentarios extras
  * 
- * 1.- Tomaremos el valor que el usuario vaya colocando en el textarea y esa valor lo vamos ir almacenando el una variable local
+ * 1.- Una vez pasada la validación, debemos ir guardando lo que escribe el usuario en el array de tweet
  * 
- * 2.- La idea siempre es validar que haya algo en el formulario al momento de enviarlo, y esa validacion al entrar de en ella tambien debes asegurarte que no continue con el codigo.
+ * 2.- En vez de agregar cada tweet en el array, colocaremos un objeto donde contenga el mesaje y un identificador unico, que lo traemos de Date.now().
  * 
- * 3.- Llegamos al momento de logica y es momento de separarla de la función actual y hacerlo en otra función.
+ * 3.- Ya cada tweet en el array, debemos proceder a mostrarlo, como es logica de HTML, una vez más lo ideal es separlo en otra funcion.
  * 
- * 4.- En esta función de mostrar un error, creamos un elementos, incluimos clases para el aspecto visual, y creamos un variable local, para tomarla como referencia al momento de inyectar este error para cuando se ejecute.
+ * 4.- En esta función crearHTML, validamos si hay algun elemento en el array, ahora te preguntaras, que la primera vez que la utlizamos ya nos aseguramos haya un elemento con la linea anterior, y esto se debe a que esta Fn crearHTML, la utlizaremos en otros lados y si o si debemos vericar que haya algo
+ * 
+ * 5.- Cuando estamo creanod el HTML que se mostrar, su ultima funcionalida es agreagrlos al HTML cierto ?, bueno como recordaras .appendChild; no borrar elementos previos, solo va incorporando, asi que optmizamos esta parte con una funcion que limpia la iteracio previa.
+ * 
+ * 6.- Viendo que cada vez que que igresamos un tweet, no se reinicia, usamos el metodo asociado a un formulario, como lo es reset.
  *  
  */
